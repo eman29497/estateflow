5adc7fb1a2e3f1c9a94ca2c3976cabb2c7d63d87
eman <emansajjad863@gmail.com>
2026-07-16 22:58:58 +0500

    Update CORS for production

--- backend/index.ts
+++ backend/index.ts
@@ -6,24 +6,29 @@ import authRoutes from "./routes/authRoutes";
 import inquiryRoutes from "./routes/inquiryRoutes";
 
 dotenv.config();
+
 const app: Application = express();
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+
+app.use(cors({
+  origin:   "https://estateflow-five-tau.vercel.app",
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
     message: "Real Estate Backend Running Successfully",
   });
 });
+
 app.use("/api/properties", propertyRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/inquiries", inquiryRoutes);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+const PORT = process.env.PORT || 10000;
+app.listen(Number(PORT), '0.0.0.0', () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
